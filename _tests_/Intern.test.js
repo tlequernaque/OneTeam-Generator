e4c8f37d03dfb53f43e964bb80fd2a0de4114a82
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-03-12 01:54:10 -0500

    added tests for intern

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,1 +1,25 @@
 const Intern = require('../lib/Intern')
+
+test("testing for name property", function(){
+    const testValue = "john";
+    const newEmployee = new Employee(testValue, "", "","");
+    expect(newEmployee.name).toBe(testValue);
+})
+
+test("testing for ID property", function(){
+    const testValue = "7";
+    const newEmployee = new Employee("", testValue, "","");
+    expect(newEmployee.id).toBe(testValue);
+})
+
+test("testing for email property", function(){
+    const testValue = "john@yahoo.com";
+    const newEmployee = new Employee("", "", testValue,"");
+    expect(newEmployee.email).toBe(testValue);
+})
+
+test("testing for GitHub property", function(){
+    const testValue = "GaTech";
+    const newEmployee = new Employee("", "","",testValue);
+    expect(newEmployee.school).toBe(testValue);
+})
