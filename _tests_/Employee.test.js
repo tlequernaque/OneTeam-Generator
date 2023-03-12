4104415c7b3a2567906286017dcb46c3204e8d59
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-03-12 01:44:49 -0500

    added code for employee test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,1 +1,19 @@
 const Employee = require('../lib/Employee');
+
+test("testing for name property", function(){
+    const testValue = "john";
+    const newEmployee = new Employee(testValue, "", "");
+    expect(newEmployee.name).toBe(testValue);
+})
+
+test("testing for ID property", function(){
+    const testValue = "7";
+    const newEmployee = new Employee("", testValue, "");
+    expect(newEmployee.id).toBe(testValue);
+})
+
+test("testing for email property", function(){
+    const testValue = "john@yahoo.com";
+    const newEmployee = new Employee("", "", testValue);
+    expect(newEmployee.email).toBe(testValue);
+})
