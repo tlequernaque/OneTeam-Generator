33832760a86d146e59736cc2ebe0aa20b16ca030
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-03-12 03:21:33 -0400

    changed code to make enginner tests pass

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,26 +1,25 @@
 const Engineer = require('../lib/Engineer');
-const Employee = require('../lib/Employee');
 
 test("testing for name property", function(){
     const testValue = "john";
-    const newEmployee = new Employee(testValue, "", "","");
+    const newEmployee = new Engineer(testValue, "", "","");
     expect(newEmployee.name).toBe(testValue);
-})
+});
 
 test("testing for ID property", function(){
     const testValue = "7";
-    const newEmployee = new Employee("", testValue, "","");
+    const newEmployee = new Engineer("", testValue, "","");
     expect(newEmployee.id).toBe(testValue);
-})
+});
 
 test("testing for email property", function(){
     const testValue = "john@yahoo.com";
-    const newEmployee = new Employee("", "", testValue,"");
+    const newEmployee = new Engineer("", "", testValue,"");
     expect(newEmployee.email).toBe(testValue);
-})
+});
 
 test("testing for GitHub property", function(){
     const testValue = "Johnj";
-    const newEmployee = new Employee("", "","",testValue);
+    const newEmployee = new Engineer("", "","",testValue);
     expect(newEmployee.github).toBe(testValue);
-})
+});
--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -5,22 +5,22 @@ test("testing for name property", function(){
     const testValue = "john";
     const newEmployee = new Employee(testValue, "", "","");
     expect(newEmployee.name).toBe(testValue);
-})
+});
 
 test("testing for ID property", function(){
     const testValue = "7";
     const newEmployee = new Employee("", testValue, "","");
     expect(newEmployee.id).toBe(testValue);
-})
+});
 
 test("testing for email property", function(){
     const testValue = "john@yahoo.com";
     const newEmployee = new Employee("", "", testValue,"");
     expect(newEmployee.email).toBe(testValue);
-})
+});
 
-test("testing for GitHub property", function(){
+test("testing for school property", function(){
     const testValue = "GaTech";
     const newEmployee = new Employee("", "","",testValue);
     expect(newEmployee.school).toBe(testValue);
-})
+});
