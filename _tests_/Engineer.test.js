10149b51c5e07c6aa1996c3b0de998f3fe9940c9
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-03-12 01:50:52 -0500

    added tests for engineer

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,1 +1,25 @@
-const Engineer = require('../lib/Engineer')
+const Engineer = require('../lib/Engineer');
+
+test("testing for name property", function(){
+    const testValue = "john";
+    const newEmployee = new Employee(testValue, "", "","");
+    expect(newEmployee.name).toBe(testValue);
+})
+
+test("testing for ID property", function(){
+    const testValue = "7";
+    const newEmployee = new Employee("", testValue, "","");
+    expect(newEmployee.id).toBe(testValue);
+})
+
+test("testing for email property", function(){
+    const testValue = "john@yahoo.com";
+    const newEmployee = new Employee("", "", testValue,"");
+    expect(newEmployee.email).toBe(testValue);
+})
+
+test("testing for GitHub property", function(){
+    const testValue = "Johnj";
+    const newEmployee = new Employee("", "","",testValue);
+    expect(newEmployee.github).toBe(testValue);
+})
