5c65273bf1c8e893016717dcd6169d6c67d050c0
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-03-12 03:25:36 -0400

    changed code to make rest of tests pass

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,26 +1,25 @@
 const Intern = require('../lib/Intern');
-const Employee = require('../lib/Employee');
 
 test("testing for name property", function(){
     const testValue = "john";
-    const newEmployee = new Employee(testValue, "", "","");
+    const newEmployee = new Intern(testValue, "", "","");
     expect(newEmployee.name).toBe(testValue);
 });
 
 test("testing for ID property", function(){
     const testValue = "7";
-    const newEmployee = new Employee("", testValue, "","");
+    const newEmployee = new Intern("", testValue, "","");
     expect(newEmployee.id).toBe(testValue);
 });
 
 test("testing for email property", function(){
     const testValue = "john@yahoo.com";
-    const newEmployee = new Employee("", "", testValue,"");
+    const newEmployee = new Intern("", "", testValue,"");
     expect(newEmployee.email).toBe(testValue);
 });
 
 test("testing for school property", function(){
     const testValue = "GaTech";
-    const newEmployee = new Employee("", "","",testValue);
+    const newEmployee = new Intern("", "","",testValue);
     expect(newEmployee.school).toBe(testValue);
 });
--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,26 +1,25 @@
 const Manager = require('../lib/Manager');
-const Employee = require('../lib/Employee');
 
 test("testing for name property", function(){
     const testValue = "john";
-    const newEmployee = new Employee(testValue, "", "","");
+    const newEmployee = new Manager(testValue, "", "","");
     expect(newEmployee.name).toBe(testValue);
 });
 
 test("testing for ID property", function(){
     const testValue = "7";
-    const newEmployee = new Employee("", testValue, "","");
+    const newEmployee = new Manager("", testValue, "","");
     expect(newEmployee.id).toBe(testValue);
 });
 
 test("testing for email property", function(){
     const testValue = "john@yahoo.com";
-    const newEmployee = new Employee("", "", testValue,"");
+    const newEmployee = new Manager("", "", testValue,"");
     expect(newEmployee.email).toBe(testValue);
 });
 
 test("testing for office number property", function(){
     const testValue = "4";
-    const newEmployee = new Employee("", "","",testValue);
+    const newEmployee = new Manager("", "","",testValue);
     expect(newEmployee.officeNumber).toBe(testValue);
 });
